35a00b1cd804711c5e0c02c9a1dfe5400a7f4d19
Andrey-Ba-developer <and.balyaeff@yandex.ru>
2023-01-10 21:09:59 +0700

    multi-language site

---
+++ app.js
@@ -0,0 +1,32 @@
+const select = document.querySelector('select');
+const allLang = ['en', 'ru'];
+
+
+select.addEventListener('change', changeURLLanguage);
+
+// перенаправить на url с указанием языка
+function changeURLLanguage() {
+	let lang = select.value;
+	location.href = window.location.pathname + '#' + lang;
+	location.reload();
+}
+
+function changeLanguage() {
+	let hash = window.location.hash;
+	hash = hash.substr(1);
+	if (!allLang.includes(hash)) {
+		location.href = window.location.pathname + '#en';
+		location.reload();
+	}
+	select.value = hash;
+	document.querySelector('.lng-intro__title').innerHTML = langArr['intro__title'][hash];
+	for (let key in langArr) {
+		let elem = document.querySelector('.lng-' + key);
+		if (elem) {
+			elem.innerHTML = langArr[key][hash];
+		}
+	}
+}
+
+
+changeLanguage();
